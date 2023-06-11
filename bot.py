0d91360b3127642af97aede972eaeed1058d0403
ohajayakan <130837471+anjaymylord@users.noreply.github.com>
2023-06-12 00:36:46 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,67 +1,60 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from io import BytesIO
-from PIL import Image
-from pyzbar import pyzbar
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-TOKEN = '6171063049:AAEtT8LfoUS6xqV2rCQlA_M_7DAse0e_zG0'
+# Inisialisasi token bot Telegram
+telegram_token = '6171063049:AAEtT8LfoUS6xqV2rCQlA_M_7DAse0e_zG0'
+bot = telegram.Bot(token=telegram_token)
 
-def start(update: Update, context):
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text='Silakan kirimkan foto QRIS untuk pembayaran.')
+# Fungsi untuk menangani perintah /start
+def start(update, context):
+    update.message.reply_text('Halo! Untuk mengakses grup ini, silakan langganan ke saluran terlebih dahulu.')
 
-def process_qris_photo(update: Update, context):
-    chat_id = update.message.chat_id
-
-    # Memeriksa apakah pesan yang diterima memiliki foto
-    if update.message.photo:
-        photo = update.message.photo[-1]  # Mengambil foto terakhir yang dikirim
-        file = context.bot.get_file(photo.file_id)
-        image_bytes = BytesIO()
-        file.download(out=image_bytes)  # Mengunduh foto QRIS
-
-        qris_data = read_qris(image_bytes)  # Membaca data QRIS dari foto
-
-        if qris_data:
-            # Lakukan verifikasi pembayaran berdasarkan data QRIS yang didapat
-            # ...
-
-            # Jika pembayaran valid, kirimkan tautan channel kepada pengguna
-            send_channel_link(chat_id)
-        else:
-            bot.send_message(chat_id=chat_id, text='Foto QRIS tidak valid.')
+# Fungsi untuk menangani pesan yang diterima dari pengguna
+def handle_message(update, context):
+    user_id = update.message.from_user.id
+    channel_username = '@mediasayu'  # Ganti dengan username saluran yang diinginkan
+    subscribed = bot.get_chat_member(channel_username, user_id).status == 'member'
+    
+    if subscribed:
+        update.message.reply_text('Anda sudah berlangganan ke saluran. Selamat datang di grup!')
     else:
-        bot.send_message(chat_id=chat_id, text='Mohon kirimkan foto QRIS.')
+        update.message.reply_text('Silakan langganan ke saluran terlebih dahulu untuk mengakses grup.')
 
-def read_qris(image_bytes):
-    # Membaca data QRIS dari foto menggunakan library pyzbar
-    image = Image.open(image_bytes)
-    qris_codes = pyzbar.decode(image)
-
-    if qris_codes:
-        qris_data = qris_codes[0].data.decode('utf-8')
-        return qris_data
+# Fungsi untuk menangani callback query
+def handle_callback_query(update, context):
+    query = update.callback_query
+    user_id = query.from_user.id
+    message = query.message
+    
+    # Cek apakah pesan merupakan pesan forward
+    if message.forward_from:
+        bot.answer_callback_query(query.id, text='Anda tidak diizinkan meneruskan pesan ini.')
+        # Jika pesan merupakan pesan forward, lakukan tindakan yang diinginkan (misalnya, hapus pesan)
+        bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
     else:
-        return None
+        bot.answer_callback_query(query.id, text='Pesan tidak diteruskan.')
 
-def send_channel_link(chat_id):
-    channel_link = 'https://t.me/+0g8h6exjM942ZGVl'  # Ganti dengan tautan channel Anda
-    message = f'Anda telah berhasil membayar. Silakan klik tautan ini untuk bergabung dengan channel kami: {channel_link}'
-    bot.send_message(chat_id=chat_id, text=message)
-
+# Fungsi utama untuk menjalankan bot Telegram
 def main():
-    updater = Updater(token=TOKEN, use_context=True)
+    updater = Updater(token=telegram_token, use_context=True)
     dispatcher = updater.dispatcher
-
+    
+    # Handler perintah /start
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-
-    qris_photo_handler = MessageHandler(Filters.photo, process_qris_photo)
-    dispatcher.add_handler(qris_photo_handler)
-
+    
+    # Handler pesan yang diterima
+    message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+    dispatcher.add_handler(message_handler)
+    
+    # Handler callback query
+    callback_query_handler = CallbackQueryHandler(handle_callback_query)
+    dispatcher.add_handler(callback_query_handler)
+    
     updater.start_polling()
     updater.idle()
 
+# Menjalankan bot Telegram
 if __name__ == '__main__':
-    bot = Bot(token=TOKEN)
     main()
