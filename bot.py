7c53a5700fc4182fef203fa73392fc0fe3580ad5
ohajayakan <130837471+anjaymylord@users.noreply.github.com>
2023-06-11 16:10:14 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,47 +1,82 @@
 import telegram
+from telegram.ext import Updater, CommandHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from datetime import datetime, timedelta
+import requests
 
+# Token bot Telegram Anda
 TOKEN = '6171063049:AAEtT8LfoUS6xqV2rCQlA_M_7DAse0e_zG0'
-bot = telegram.Bot(token=TOKEN)
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
 
+# Token API pembayaran QRIS
+QRIS_TOKEN = 'ID2022147421889A01'
+
+# Fungsi untuk menangani perintah /start
 def start(update, context):
-    chat_id = update.message.chat_id
+    user = update.effective_user
+    chat_id = update.effective_chat.id
+    
+    # Generate QR code pembayaran
+    qr_code = generate_qr_code(chat_id)
+    
+    # Kirim QR code sebagai foto
+    context.bot.send_photo(chat_id=chat_id, photo=qr_code)
+    
+    # Kirim pesan dengan instruksi pembayaran
+    context.bot.send_message(chat_id=chat_id, text='Silakan lakukan pembayaran melalui QR code di atas.')
+    context.bot.send_message(chat_id=chat_id, text='Setelah pembayaran berhasil, Anda akan menerima tautan channel khusus.')
+    
 
-    # Buat tautan unik dengan menambahkan timestamp saat ini
-    link = f"https://t.me/paymentryubot?start=unique_code_{datetime.now().timestamp()}"
+# Fungsi untuk menghasilkan QR code pembayaran menggunakan API QRIS
+def generate_qr_code(chat_id):
+    # URL API QRIS
+    api_url = 'https://api.qris.com/generate_qr_code'
+    
+    # Data pembayaran yang akan dikirim ke API QRIS
+    payment_data = {
+        'chat_id': chat_id,
+        'amount': 100,  # Jumlah pembayaran dalam rupiah
+        'description': 'Pembayaran Bergabung ke Channel Khusus'  # Deskripsi pembayaran
+    }
+    
+    # Kirim permintaan POST ke API QRIS
+    response = requests.post(api_url, json=payment_data, headers={'Authorization': f'Bearer {QRIS_TOKEN}'})
+    
+    # Ambil QR code dari respons API
+    qr_code_url = response.json().get('qr_code')
+    
+    # Unduh QR code sebagai file
+    qr_code = requests.get(qr_code_url).content
+    
+    return qr_code
+    
 
-    # Tambahkan tautan ke keyboard inline
-    keyboard = [[InlineKeyboardButton("Join Channel", url=link)]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
+# Fungsi untuk menangani perintah /verify_payment
+def verify_payment(update, context):
+    chat_id = update.effective_chat.id
+    
+    # Lakukan verifikasi pembayaran
+    
+    # Jika pembayaran berhasil, berikan tautan channel khusus
+    channel_link = 'https://t.me/nama_channel_khusus'
+    context.bot.send_message(chat_id=chat_id, text=f'Pembayaran berhasil. Berikut adalah tautan channel khusus: {channel_link}')
+    
 
-    # Kirim pesan dengan keyboard inline
-    response_message = "Klik tombol berikut untuk bergabung dengan channel:"
-    context.bot.send_message(chat_id=chat_id, text=response_message, reply_markup=reply_markup)
+def main():
+    # Inisialisasi bot
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    # Tambahkan handler perintah /start
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    
+    # Tambahkan handler perintah /verify_payment
+    verify_payment_handler = CommandHandler('verify_payment', verify_payment)
+    dispatcher.add_handler(verify_payment_handler)
+    
+    # Jalankan bot
+    updater.start_polling()
+    updater.idle()
 
-def join_channel(update, context):
-    chat_id = update.message.chat_id
 
-    # Periksa apakah tautan masih berlaku
-    link_timestamp = float(update.message.text.split('_')[-1])
-    expiration_time = datetime.fromtimestamp(link_timestamp) + timedelta(hours=1)
-    current_time = datetime.now()
-
-    if current_time < expiration_time:
-        response_message = "Selamat bergabung dengan channel!"
-        # Lakukan tindakan lain yang diinginkan, misalnya menambahkan pengguna ke database
-    else:
-        response_message = "Maaf, tautan sudah kedaluwarsa."
-
-    context.bot.send_message(chat_id=chat_id, text=response_message)
-
-start_handler = CommandHandler('start', start)
-join_channel_handler = MessageHandler(Filters.text & Filters.regex(r'^unique_code_'), join_channel)
-
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(join_channel_handler)
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
