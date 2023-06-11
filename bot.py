03b5b0d0b489c96ce50db57c1172c512f77c0699
ohajayakan <130837471+anjaymylord@users.noreply.github.com>
2023-06-11 15:46:52 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,101 +1,47 @@
-import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+import telegram
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime, timedelta
 
-# Konfigurasi token bot Telegram Anda
-TOKEN = 'your_bot_token'
+TOKEN = '6171063049:AAEtT8LfoUS6xqV2rCQlA_M_7DAse0e_zG0'
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-# Konfigurasi identitas dan data pembayaran menggunakan DANA
-PAYMENT_PROVIDER_TOKEN = 'your_payment_provider_token'
-PAYMENT_CURRENCY = 'IDR'
-PAYMENT_DESCRIPTION = 'Payment for product'
-PAYMENT_SUCCESS_URL = 'https://your_website.com/success'
+def start(update, context):
+    chat_id = update.message.chat_id
 
-# Mengatur log level
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+    # Buat tautan unik dengan menambahkan timestamp saat ini
+    link = f"https://t.me/paymentryubot?start=unique_code_{datetime.now().timestamp()}"
 
-logger = logging.getLogger(__name__)
+    # Tambahkan tautan ke keyboard inline
+    keyboard = [[InlineKeyboardButton("Join Channel", url=link)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
+    # Kirim pesan dengan keyboard inline
+    response_message = "Klik tombol berikut untuk bergabung dengan channel:"
+    context.bot.send_message(chat_id=chat_id, text=response_message, reply_markup=reply_markup)
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Halo! Selamat datang di bot pembayaran kami. Silakan klik tombol bayar untuk melanjutkan pembayaran.')
+def join_channel(update, context):
+    chat_id = update.message.chat_id
 
-    # Membuat tombol pembayaran menggunakan DANA
-    button = InlineKeyboardButton(text='Bayar dengan DANA',
-                                  callback_data='pay_dana')
-    keyboard = InlineKeyboardMarkup([[button]])
+    # Periksa apakah tautan masih berlaku
+    link_timestamp = float(update.message.text.split('_')[-1])
+    expiration_time = datetime.fromtimestamp(link_timestamp) + timedelta(hours=1)
+    current_time = datetime.now()
 
-    update.message.reply_text('Klik tombol di bawah untuk melanjutkan pembayaran.', reply_markup=keyboard)
-
-
-def button_callback(update: Update, context: CallbackContext) -> None:
-    """Handle button callback queries."""
-    query = update.callback_query
-    query.answer()
-
-    if query.data == 'pay_dana':
-        # Mendapatkan nomor DANA Rupiah pembeli
-        dana_number = '089517434967'  # Ganti dengan nomor DANA Rupiah pembeli yang sesuai
-
-        # Menggunakan library python-payment untuk menghasilkan URL pembayaran menggunakan DANA
-        payment_url = generate_dana_payment_url(dana_number)
-
-        # Menyimpan URL pembayaran dalam variabel global agar dapat diakses setelah pembayaran berhasil
-        context.user_data['payment_url'] = payment_url
-
-        # Mengirim URL pembayaran kepada pengguna
-        query.message.reply_text(f'Silakan lakukan pembayaran dengan mengklik tautan berikut: {payment_url}')
-
-
-def generate_dana_payment_url(dana_number: str) -> str:
-    # Implementasikan logika untuk menghasilkan URL pembayaran menggunakan DANA di sini
-    # Gunakan library python-payment atau library HTTP/HTTPS yang sesuai untuk membuat URL pembayaran
-
-    # Contoh sederhana
-    url = f'https://your_payment_gateway.com/dana-payment-url?dana_number={dana_number}'
-
-    return url
-
-
-def payment_success(update: Update, context: CallbackContext) -> None:
-    """Handle payment success after user completes the payment."""
-    # Mengambil URL pembayaran dari variabel global
-    payment_url = context.user_data.get('payment_url')
-
-    if payment_url:
-        # Menyalin URL pembayaran ke clipboard atau tempat penyimpanan lainnya
-        # Anda dapat menggunakan library seperti pyperclip untuk menyalin URL ke clipboard
-
-        update.message.reply_text('Pembayaran berhasil! Tautan sekali join telah disalin.')
-
-        # Hapus URL pembayaran dari variabel global setelah menyalin
-        del context.user_data['payment_url']
+    if current_time < expiration_time:
+        response_message = "Selamat bergabung dengan channel!"
+        # Lakukan tindakan lain yang diinginkan, misalnya menambahkan pengguna ke database
     else:
-        update.message.reply_text('Pembayaran berhasil!')
+        response_message = "Maaf, tautan sudah kedaluwarsa."
 
+    context.bot.send_message(chat_id=chat_id, text=response_message)
 
-def main() -> None:
-    """Run the bot."""
-    # Inisialisasi objek Dispatcher
-    dispatcher = Dispatcher(bot, update_queue=None, workers=0, use_context=True)
+start_handler = CommandHandler('start', start)
+join_channel_handler = MessageHandler(Filters.text & Filters.regex(r'^unique_code_'), join_channel)
 
-    # Menambahkan handler untuk command /start
-    dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(join_channel_handler)
 
-    # Menambahkan handler untuk tombol pembayaran
-    dispatcher.add_handler(CallbackQueryHandler(button_callback))
-
-    # Menambahkan handler untuk tindakan pembayaran berhasil
-    dispatcher.add_handler(CommandHandler('payment_success', payment_success))
-
-    # Memulai polling bot Telegram
-    dispatcher.start_polling()
-
-    # Mengecek apakah ada perintah yang diberikan kepada bot
-    dispatcher.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
